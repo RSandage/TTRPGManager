47a8ca053700a7900702a84b3ab74eee46955096
RSandage <90716032+RSandage@users.noreply.github.com>
2022-05-31 16:18:44 -0500

    Added imports for AuthModule, deleted imports of removed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { EncounterComponent } from './encounter/encounter.component';
-import { UserComponent } from './user/user.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
-import { UserFormComponent } from './user/user-form/user-form.component';
-import { LoginFormComponent } from './user/login-form/login-form.component';
+import { AuthModule } from './auth/auth.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    EncounterComponent,
-    UserComponent,
     HomeComponent,
-    UserFormComponent,
-    LoginFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
