ca80da6bcddde7a62f7176bbda1263b9e9389ad1
RSandage <90716032+RSandage@users.noreply.github.com>
2022-06-01 12:12:19 -0500

    Adding dropdown menu for profile

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    if (this.auth.user) {
+      this.auth.logout()
+      this.router.navigate(['/home'])
+    }
+    //do nothing
   }
 
 }
