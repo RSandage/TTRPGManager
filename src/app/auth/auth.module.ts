ebfab1fa2ce6854cb456ee057bf4e7a95433a14c
RSandage <90716032+RSandage@users.noreply.github.com>
2022-06-01 12:10:52 -0500

    Created and declared profile component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from "./components/register/register.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { LoginComponent } from "./components/login/login.component";
+import { ProfileComponent } from './component/profile/profile.component';
 
 
 @NgModule({
-  declarations: [RegisterComponent, LoginComponent],
+  declarations: [RegisterComponent, LoginComponent, ProfileComponent],
   imports: [CommonModule, AuthRoutingModule, FormsModule, ReactiveFormsModule],
   exports: [RegisterComponent, LoginComponent],
 })
