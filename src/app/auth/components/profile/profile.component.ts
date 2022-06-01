8b629fe0d2efccf5165c1f78d3b6364c8925ec2d
RSandage <90716032+RSandage@users.noreply.github.com>
2022-06-01 12:11:13 -0500

    Create profile.component.ts

---
+++ src/app/auth/components/profile/profile.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../../models/user.model';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+
+  constructor(private auth: AuthService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+}
